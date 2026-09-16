f4d9b61d46c2f0e83e2a41e49fdfa4c8f4976921
Krzysztof Foltman <wdev@foltman.com>
2026-09-16 19:16:55 +0200

    Add simple text drawing using a hardcoded pixel dump of Bitstream Vera Sans.

--- include/display.h
+++ include/display.h
@@ -15,5 +15,5 @@ public:
     virtual void complete() {}
     virtual void fill(const Rect &rect, RGB565 colour) = 0;
     virtual void copy(const Rect &rect, const RGB565 *src) = 0;
-    virtual void copy1bit(const Point &pt, int pixels, const uint8_t *src, int x_offset, int pitch, RGB565 bg, RGB565 fg) = 0;
+    virtual void copy1bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, RGB565 bg, RGB565 fg) = 0;
 };
