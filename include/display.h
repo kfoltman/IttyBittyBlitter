755fc153254955b789661b6419068ed774febd8c
Krzysztof Foltman <wdev@foltman.com>
2026-09-20 20:10:36 +0100

    Make blitter routines reusable across display implementations.

--- include/display.h
+++ include/display.h
@@ -18,3 +18,83 @@ public:
     virtual void copy1bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, RGB565 bg, RGB565 fg) = 0;
     virtual void copy4bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, const RGB565 *cmap) = 0;
 };
+
+template<typename T>
+class BaseDisplayOps: public BaseDisplay
+{
+public:
+    BaseDisplayOps(int16_t _w, int16_t _h) : BaseDisplay(_w, _h) {}
+    void fill(const Rect &rect, RGB565 colour) {
+        struct Solid {
+            uint16_t val;
+            inline uint16_t next() const { return val; }
+            inline void line(int) const {}
+            inline void skip(int) const {}
+        };
+        Solid solid{colour.val()};
+        T::output(this, rect, solid);
+    }
+    void copy(const Rect &rect, const RGB565 *src) {
+        struct Slurp {
+            const RGB565 *src;
+            int pos;
+            int pitch;
+            inline uint16_t next() { return src[pos++].val(); }
+            inline void skip(int count) {
+                pos += count;
+            }
+            inline void line(int count) {
+                pos = 0;
+                src += count * pitch;
+            }
+        };
+        Slurp slurp{src, 0, rect.width()};
+        T::output(this, rect, slurp);
+    }
+    void copy1bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, RGB565 bg, RGB565 fg) {
+        struct Slurp {
+            const uint8_t *glyphs;
+            int pos0, pos;
+            int pitch;
+            uint16_t bg;
+            uint16_t fg;
+            inline uint16_t next() {
+                uint16_t rv = (glyphs[pos >> 3] << (pos & 7)) & 128 ? fg : bg;
+                pos++;
+                return rv;
+            }
+            inline void skip(int count) {
+                pos += count;
+            }
+            inline void line(int count) {
+                glyphs += count * pitch;
+                pos = pos0;
+            }
+        };
+        Slurp slurp{src, x_offset, x_offset, pitch, bg.val(), fg.val()};
+        T::output(this, Rect(pt.x, pt.y, pt.x + pixels, pt.y + height), slurp);
+    }
+    void copy4bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, const RGB565 *cmap) {
+        struct Slurp {
+            const uint8_t *glyphs;
+            int pos0, pos;
+            int pitch;
+            const RGB565 *cmap;
+            inline uint16_t next() {
+                uint8_t alpha = (glyphs[pos >> 1] >> (((~pos) & 1) << 2)) & 0xF;
+                uint16_t rv = cmap[alpha].val();
+                pos++;
+                return rv;
+            }
+            inline void skip(int count) {
+                pos += count;
+            }
+            inline void line(int count) {
+                glyphs += count * pitch;
+                pos = pos0;
+            }
+        };
+        Slurp slurp{src, x_offset, x_offset, pitch, cmap};
+        T::output(this, Rect(pt.x, pt.y, pt.x + pixels, pt.y + height), slurp);
+    }
+};
--- src/pcmain.cpp
+++ src/pcmain.cpp
@@ -1,15 +1,11 @@
-#include <algorithm>
-#include <functional>
 #include <assert.h>
 #include <math.h>
-#include <time.h>
 #include <unistd.h>
 #include <SDL/SDL.h>
-#include <iostream>
 #include "display.h"
 #include "fonts.h"
 
-class SDLDisplay: public BaseDisplay
+class SDLDisplay: public BaseDisplayOps<SDLDisplay>
 {
 private:
     int zoom;
@@ -17,7 +13,7 @@ private:
     Rect clip;
 public:
     SDLDisplay(int _w, int _h, int _zoom = 1)
-    : BaseDisplay(_w, _h)
+    : BaseDisplayOps(_w, _h)
     , zoom(_zoom)
     , clip(0, 0, _w, _h)
     {}
@@ -31,7 +27,11 @@ public:
         clip = Rect(std::max<int16_t>(0, cr.left()), std::max<int16_t>(0, cr.top()), std::min<int16_t>(w, cr.right()), std::min<int16_t>(h, cr.bottom()));
     }
     template<typename Gen>
-    void output(const Rect &rect, Gen &gen) {
+    static void output(BaseDisplayOps<SDLDisplay> *obj, const Rect &rect, Gen &gen) {
+        auto *self = static_cast<SDLDisplay *>(obj);
+        SDL_Surface *pc_display = self->pc_display;
+        auto &clip = self->clip;
+        auto zoom = self->zoom;
         if (rect.width() < 0 || rect.height() < 0)
             return;
         int yt = std::max(rect.top(), clip.top());
@@ -67,79 +67,6 @@ public:
             }
             SDL_UnlockSurface(pc_display);
         }
-    }
-    void fill(const Rect &rect, RGB565 colour) {
-        struct Solid {
-            uint16_t val;
-            inline uint16_t next() const { return val; }
-            inline void line(int) const {}
-            inline void skip(int) const {}
-        };
-        Solid solid{colour.val()};
-        output(rect, solid);
-    }
-    void copy(const Rect &rect, const RGB565 *src) {
-        struct Slurp {
-            const RGB565 *src;
-            int pos;
-            int pitch;
-            inline uint16_t next() { return src[pos++].val(); }
-            inline void skip(int count) {
-                pos += count;
-            }
-            inline void line(int count) {
-                pos = 0;
-                src += count * pitch;
-            }
-        };
-        Slurp slurp{src, 0, rect.width()};
-        output(rect, slurp);
-    }
-    void copy1bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, RGB565 bg, RGB565 fg) {
-        struct Slurp {
-            const uint8_t *glyphs;
-            int pos0, pos;
-            int pitch;
-            uint16_t bg;
-            uint16_t fg;
-            inline uint16_t next() {
-                uint16_t rv = (glyphs[pos >> 3] << (pos & 7)) & 128 ? fg : bg;
-                pos++;
-                return rv;
-            }
-            inline void skip(int count) {
-                pos += count;
-            }
-            inline void line(int count) {
-                glyphs += count * pitch;
-                pos = pos0;
-            }
-        };
-        Slurp slurp{src, x_offset, x_offset, pitch, bg.val(), fg.val()};
-        output(Rect(pt.x, pt.y, pt.x + pixels, pt.y + height), slurp);
-    }
-    void copy4bit(const Point &pt, int pixels, int height, const uint8_t *src, int x_offset, int pitch, const RGB565 *cmap) {
-        struct Slurp {
-            const uint8_t *glyphs;
-            int pos0, pos;
-            int pitch;
-            const RGB565 *cmap;
-            inline uint16_t next() {
-                uint8_t alpha = (glyphs[pos >> 1] >> (((~pos) & 1) << 2)) & 0xF;
-                uint16_t rv = cmap[alpha].val();
-                pos++;
-                return rv;
-            }
-            inline void skip(int count) {
-                pos += count;
-            }
-            inline void line(int count) {
-                glyphs += count * pitch;
-                pos = pos0;
-            }
-        };
-        Slurp slurp{src, x_offset, x_offset, pitch, cmap};
-        output(Rect(pt.x, pt.y, pt.x + pixels, pt.y + height), slurp);
     }
 };
 
